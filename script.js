15083457faa0f5b18bb7536619015b02b9e060ea
Aimeri Baddouh <aimeri@baddouh.me>
2017-09-21 18:54:47 -0400

    randomizing bg pics

--- script.js
+++ script.js
@@ -7,10 +7,37 @@ let createGrid = function(sizeOfGrid) {
     };
 };
 
-let slothify = function(sizeOfGrid) {
-    for (i = 0; i < sizeOfGrid; i++){
-        let currentDiv = document.body.getElementsByClassName('box' + i)[0]
-        console.log(currentDiv);
-        currentDiv.style.backgroundImage = "url('./assets/" + i + ".jpg')";
-    }
+let randomizer = function() {
+    let gridSize = document.body.getElementsByClassName('boxes');
+    return Math.floor(Math.random() * gridSize.length/2);
 }
+
+let slothify = function() {
+    let gridSize = document.body.getElementsByClassName('boxes');
+    
+    for (i = 0; i < gridSize.length; i++){
+        let currentDiv = document.body.getElementsByClassName('box' + i)[0]
+        console.log(currentDiv.style.backgroundImage)
+        currentDiv.style.backgroundImage = "url('./assets/" + randomizer() + ".jpg')";
+        console.log(currentDiv.style.backgroundImage)
+
+    }
+
+/*     for (i = 0; i < gridSize.length; i++){
+        let currentDiv = document.body.getElementsByClassName('box' + i)[0]
+        if (i < gridSize.length/2) {
+            currentDiv.style.backgroundImage = "url('./assets/" + i + ".jpg')"; 
+        }
+            
+        
+        else  {
+            for (j = 0; j < gridSize.length / 2; j++){
+            currentDiv.style.backgroundImage = "url('./assets/" + j + ".jpg')";
+            console.log(currentDiv.style.backgroundImage = "url('./assets/" + j + ".jpg')")
+            }
+        }
+        
+        
+    } */
+}
+
